7d8464a1e5bd30a837099eeb466a2ae7ddb31375
Sergey Smirnov <smirnov772@gmail.com>
2020-08-13 13:13:44 +0300

    добавил кнопки

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector ('.popup');
+const popupOpenButton = document.querySelector ('.popup__open');
+const popupCloseButton = document.querySelector ('.popup__close');
+console.log ({
+    popup,
+    popupOpenButton,
+    popupCloseButton
+});
