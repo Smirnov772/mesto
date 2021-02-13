69dd5511858d889d787dfc66a267ecb526cd9c6f
Sergey Smirnov <smirnov772@gmail.com>
2021-02-13 22:09:39 +0300

    revue1.4.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import './index.css';
+// import './index.css';
 import { Section } from "../components/Section.js";
 import { Popup } from "../components/Popup.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
