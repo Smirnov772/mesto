fe099e7f03bf9743ec5ce50036d07b21091f6289
Sergey Smirnov <smirnov772@gmail.com>
2021-02-13 22:33:26 +0300

    revue1.4.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ const enableValidation = {
   inputSelector: ".popup__form-input",
   submitButtonSelector: ".popup__save-button",
   inactiveButtonClass: "popup__save-button_disabled",
-  inputErrorClass: "popup__input_type_error",
+  inputErrorClass: "popup__form-input_type_error",
   errorClass: "popup__error_visible",
 };
 
