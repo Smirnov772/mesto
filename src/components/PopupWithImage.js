fbc413b80327ce63a2a92f529a21aa1dfb374e0e
Sergey Smirnov <smirnov772@gmail.com>
2021-02-27 01:04:52 +0300

    revue9.2

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -5,15 +5,16 @@ export class PopupWithImage extends Popup {
 
     this._imageLink = this._popupElement.querySelector(".big-image__item");
     this._imageAlt = this._popupElement.querySelector(".big-image__item");
-
+    this._imageName = this._popupElement.querySelector( 
+      ".big-image__name" );
     // this.popupImageSelector = popupImageSelector
   }
   open(data) {
     this._link = data.link;
     this._name = data.name;
-    this._imageLink.src = this.link;
-    this._imageAlt.alt = `Изображение ${this.name}`;
-    this._imageName.textContent = this.name;
+    this._imageLink.src = this._link;
+    this._imageLink.alt = `Изображение ${this._name}`;
+    this._imageName.textContent = this._name;
 
     super.open();
   }
